3b7a5f1b9087f16cf7e0f032a5d19998c44dab8c
BossForever <BossForever@117d2afa-4b10-4f97-ac9f-ac0015ab88bf>
2015-09-17 02:48:30 +0000

    Atualizações nos Npcs, Teleports, Village Master e Quests

--- CT25-DataPack/dist/game/data/scripts/ai/individual/npc/MercenaryCaptain/MercenaryCaptain.java
+++ CT25-DataPack/dist/game/data/scripts/ai/individual/npc/MercenaryCaptain/MercenaryCaptain.java
@@ -1,135 +1,231 @@
 /*
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * Copyright (C) 2004-2014 L2J DataPack
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This file is part of L2J DataPack.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package ai.individual.npc.MercenaryCaptain;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
-import ct25.xtreme.Config;
-import ct25.xtreme.gameserver.ThreadPoolManager;
-import ct25.xtreme.gameserver.instancemanager.TownManager;
-import ct25.xtreme.gameserver.model.actor.L2Character;
+import ai.engines.L2AttackableAIScript;
+import ct25.xtreme.gameserver.datatables.MultiSell;
+import ct25.xtreme.gameserver.instancemanager.TerritoryWarManager;
+import ct25.xtreme.gameserver.instancemanager.TerritoryWarManager.Territory;
+import ct25.xtreme.gameserver.instancemanager.TerritoryWarManager.TerritoryNPCSpawn;
 import ct25.xtreme.gameserver.model.actor.L2Npc;
 import ct25.xtreme.gameserver.model.actor.instance.L2PcInstance;
-import ct25.xtreme.gameserver.model.quest.Quest;
-import ct25.xtreme.gameserver.model.zone.type.L2TownZone;
 import ct25.xtreme.gameserver.network.clientpackets.Say2;
-import ct25.xtreme.gameserver.network.serverpackets.NpcSay;
+import ct25.xtreme.gameserver.network.serverpackets.ExShowDominionRegistry;
+import ct25.xtreme.gameserver.network.serverpackets.NpcHtmlMessage;
 
 /**
- * @author mrTJO, Synerge
+ * Mercenary Captain AI.
+ * @author malyelfik
  */
-public class MercenaryCaptain extends Quest
+public final class MercenaryCaptain extends L2AttackableAIScript
 {
-	// Npcs
-	private static final int CAPTAIN_GLUDIO = 36481; // Gludio
-	private static final int CAPTAIN_DION = 36482; // Dion
-	private static final int CAPTAIN_GIRAN = 36483; // Giran
-	private static final int CAPTAIN_OREN = 36484; // Oren
-	private static final int CAPTAIN_ADEN = 36485; // Aden
-	private static final int CAPTAIN_INNADRIL = 36486; // Innadril
-	private static final int CAPTAIN_GODDARD = 36487; // Goddard
-	private static final int CAPTAIN_RUNE = 36488; // Rune
-	private static final int CAPTAIN_SCHUTTGART = 36489; // Schuttgart
-	private List<L2Npc> _npcs = new ArrayList<>();
-	
-	// Strings
-	private static final String[] TEXT = 
-	{ 
-		"Do you wish to fight? Are you afraid? " + "No matter how hard you try, you have nowhere to run. " + "But if you face it head on, our mercenary troop will " + "help you out!", 
-		"Courage! Ambition! Passion! " + "Mercenaries who want to realize their dream of " + "fighting in the territory war, come to me! Fortune and " + "glory are waiting for you!", 
-		"Do you wish to fight? Are you afraid? " + "No matter how hard you try, you have nowhere to run. " + "But if you face it head on, our mercenary troop will " + "help you out!" 
-	};
-	
-	public MercenaryCaptain(int questId, String name, String descr)
+	// NPCs
+	private static final Map<Integer, Integer> NPCS = new HashMap<>();
+	static
 	{
-		super(questId, name, descr);
-		
-		addSpawnId(CAPTAIN_GLUDIO);
-		addSpawnId(CAPTAIN_DION);
-		addSpawnId(CAPTAIN_GIRAN);
-		addSpawnId(CAPTAIN_OREN);
-		addSpawnId(CAPTAIN_ADEN);
-		addSpawnId(CAPTAIN_INNADRIL);
-		addSpawnId(CAPTAIN_GODDARD);
-		addSpawnId(CAPTAIN_RUNE);
-		addSpawnId(CAPTAIN_SCHUTTGART);
-		
-		// Schedule Broadcast for Shout
-		scheduleBroadcast();
+		NPCS.put(36481, 13757); // Mercenary Captain (Gludio)
+		NPCS.put(36482, 13758); // Mercenary Captain (Dion)
+		NPCS.put(36483, 13759); // Mercenary Captain (Giran)
+		NPCS.put(36484, 13760); // Mercenary Captain (Oren)
+		NPCS.put(36485, 13761); // Mercenary Captain (Aden)
+		NPCS.put(36486, 13762); // Mercenary Captain (Innadril)
+		NPCS.put(36487, 13763); // Mercenary Captain (Goddard)
+		NPCS.put(36488, 13764); // Mercenary Captain (Rune)
+		NPCS.put(36489, 13765); // Mercenary Captain (Schuttgart)
 	}
+	// Items
+	private static final int STRIDER_WIND = 4422;
+	private static final int STRIDER_STAR = 4423;
+	private static final int STRIDER_TWILIGHT = 4424;
+	private static final int GUARDIAN_STRIDER = 14819;
+	private static final int ELITE_MERCENARY_CERTIFICATE = 13767;
+	private static final int TOP_ELITE_MERCENARY_CERTIFICATE = 13768;
+	// Misc
+	private static final int DELAY = 3600000; // 1 hour
+	private static final int MIN_LEVEL = 40;
+	private static final int CLASS_LEVEL = 2;
 	
-	@Override
-	public String onSpawn(L2Npc npc)
+	private MercenaryCaptain()
 	{
-		if (!_npcs.contains(npc))
-			_npcs.add(npc);
-		
-		return super.onSpawn(npc);
-	}
-	
-	private void scheduleBroadcast()
-	{
-		final Calendar cal = Calendar.getInstance();
-		final int tfhTime = cal.get(Calendar.HOUR_OF_DAY);
-		final long currTime = tfhTime * 3600000 + cal.get(Calendar.MINUTE) * 60000 + cal.get(Calendar.SECOND) * 1000 + cal.get(Calendar.MILLISECOND);
-		long nextTime = 0;
-		if ((tfhTime + 1) > 24)
-			nextTime = 6900000;
-		else if (cal.get(Calendar.MINUTE) < 55)
-			nextTime = tfhTime * 3600000 + 3300000;
-		else
-			nextTime = (tfhTime + 1) * 3600000 + 3300000;
-		
-		final long initial = nextTime - currTime;
-		ThreadPoolManager.getInstance().scheduleGeneralAtFixedRate(new BroadcastToZone(), initial, 3600000L);
-	}
-		
-	private class BroadcastToZone implements Runnable
-	{
-		@Override
-		public void run()
+		super(-1, MercenaryCaptain.class.getSimpleName(), "ai/npc");
+		for (int id : NPCS.keySet())
 		{
-			for (final L2Npc npc : _npcs)
+			addStartNpc(id);
+			addFirstTalkId(id);
+			addTalkId(id);
+		}
+		
+		for (Territory terr : TerritoryWarManager.getInstance().getAllTerritories())
+		{
+			for (TerritoryNPCSpawn spawn : terr.getSpawnList())
 			{
-				if (npc == null)
-					continue;
-
-				if (Config.DEBUG)
-					_log.info("Broadcasting Mercenary Captain Message to Zone");
-				
-				final int dg = getRandom(0, 2);
-				final NpcSay ns = new NpcSay(npc.getObjectId(), Say2.SHOUT, npc.getId(), TEXT[dg]);
-				final L2TownZone town = TownManager.getTown(npc.getX(), npc.getY(), npc.getZ());
+				if (NPCS.keySet().contains(spawn.getId()))
 				{
-					if (town.getCharactersInside() != null && !town.getCharactersInside().isEmpty())
-					{
-						for (final L2Character obj : town.getCharactersInside().values())
-						{
-							if (obj instanceof L2PcInstance)
-								obj.sendPacket(ns);
-						}
-					}
+					startQuestTimer("say", DELAY, spawn.getNpc(), null, true);
 				}
 			}
 		}
 	}
 	
+	@Override
+	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
+	{
+		String htmltext = null;
+		if (player != null)
+		{
+			final StringTokenizer st = new StringTokenizer(event, " ");
+			switch (st.nextToken())
+			{
+				case "36481-02.html":
+				{
+					htmltext = event;
+					break;
+				}
+				case "36481-03.html":
+				{
+					final NpcHtmlMessage html = new NpcHtmlMessage(npc.getObjectId());
+					html.setHtml(getHtm(player.getHtmlPrefix(), "36481-03.html"));
+					html.replace("%strider%", String.valueOf(TerritoryWarManager.MINTWBADGEFORSTRIDERS));
+					html.replace("%gstrider%", String.valueOf(TerritoryWarManager.MINTWBADGEFORBIGSTRIDER));
+					player.sendPacket(html);
+					break;
+				}
+				case "territory":
+				{
+					player.sendPacket(new ExShowDominionRegistry(npc.getCastle().getCastleId(), player));
+					break;
+				}
+				case "strider":
+				{
+					final String type = st.nextToken();
+					final int price = (type.equals("3")) ? TerritoryWarManager.MINTWBADGEFORBIGSTRIDER : TerritoryWarManager.MINTWBADGEFORSTRIDERS;
+					final int badgeId = NPCS.get(npc.getId());
+					if (getQuestItemsCount(player, badgeId) < price)
+					{
+						return "36481-07.html";
+					}
+					
+					final int striderId;
+					switch (type)
+					{
+						case "0":
+						{
+							striderId = STRIDER_WIND;
+							break;
+						}
+						case "1":
+						{
+							striderId = STRIDER_STAR;
+							break;
+						}
+						case "2":
+						{
+							striderId = STRIDER_TWILIGHT;
+							break;
+						}
+						case "3":
+						{
+							striderId = GUARDIAN_STRIDER;
+							break;
+						}
+						default:
+						{
+							_log.warning(MercenaryCaptain.class.getSimpleName() + ": Unknown strider type: " + type);
+							return null;
+						}
+					}
+					takeItems(player, badgeId, price);
+					giveItems(player, striderId, 1);
+					htmltext = "36481-09.html";
+					break;
+				}
+				case "elite":
+				{
+					if (!hasQuestItems(player, ELITE_MERCENARY_CERTIFICATE))
+					{
+						htmltext = "36481-10.html";
+					}
+					else
+					{
+						final int listId = 676 + npc.getCastle().getCastleId();
+						MultiSell.getInstance().separateAndSend(listId, player, npc, false);
+					}
+					break;
+				}
+				case "top-elite":
+				{
+					if (!hasQuestItems(player, TOP_ELITE_MERCENARY_CERTIFICATE))
+					{
+						htmltext = "36481-10.html";
+					}
+					else
+					{
+						final int listId = 685 + npc.getCastle().getCastleId();
+						MultiSell.getInstance().separateAndSend(listId, player, npc, false);
+					}
+					break;
+				}
+			}
+		}
+		else if (event.equalsIgnoreCase("say") && !npc.isDecayed())
+		{
+			if (TerritoryWarManager.getInstance().isTWInProgress())
+			{
+				broadcastNpcSay(npc, Say2.NPC_SHOUT, "Charge! Charge! Charge!");
+			}
+			else if (getRandom(2) == 0)
+			{
+				broadcastNpcSay(npc, Say2.NPC_SHOUT, "Courage! Ambition! Passion! Mercenaries who want to realize their dream of fighting in the territory war, come to me! Fortune and glory are waiting for you!");
+			}
+			else
+			{
+				broadcastNpcSay(npc, Say2.NPC_SHOUT, "Do you wish to fight? Are you afraid? No matter how hard you try, you have nowhere to run. But if you face it head on, our mercenary troop will help you out!");
+			}
+		}
+		return htmltext;
+	}
+	
+	@Override
+	public String onFirstTalk(L2Npc npc, L2PcInstance player)
+	{
+		final String htmltext;
+		if ((player.getLevel() < MIN_LEVEL) || (player.getClassId().level() < CLASS_LEVEL))
+		{
+			htmltext = "36481-08.html";
+		}
+		else if (npc.isMyLord(player))
+		{
+			htmltext = (npc.getCastle().getSiege().getIsInProgress() || TerritoryWarManager.getInstance().isTWInProgress()) ? "36481-05.html" : "36481-04.html";
+		}
+		else
+		{
+			htmltext = (npc.getCastle().getSiege().getIsInProgress() || TerritoryWarManager.getInstance().isTWInProgress()) ? "36481-06.html" : npc.getId() + "-01.html";
+		}
+		return htmltext;
+	}
+	
 	public static void main(String[] args)
 	{
-		new MercenaryCaptain(-1, "MercenaryCaptain", "npc");
+		new MercenaryCaptain();
 	}
 }
